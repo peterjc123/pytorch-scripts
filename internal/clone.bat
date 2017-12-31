1d83be9bb61b1f8042fae502ba269326a1cf18b6
peterjc123 <peter_jiachen@163.com>
2017-12-31 17:29:37 +0800

    Remove useless code

--- internal/clone.bat
+++ internal/clone.bat
@@ -3,10 +3,3 @@
 git clone --recursive https://github.com/pytorch/pytorch
 cd pytorch
 xcopy /Y aten\src\ATen\common_with_cwrap.py tools\shared\cwrap_common.py
-
-REM Before the merge of PR 3757
-mkdir torch\lib\build\ATen\src\ATen
-cd torch\lib\build\ATen\src\ATen
-mkdir ATen
-python  ../../../../../../aten/src/ATen/gen.py -s  ../../../../../../aten/src/ATen  ../../../../../../aten/src/ATen/Declarations.cwrap  ../../../../../../aten/src/THNN/generic/THNN.h  ../../../../../../aten/src/THCUNN/generic/THCUNN.h  ../../../../../../aten/src/ATen/nn.yaml  ../../../../../../aten/src/ATen/native/native_functions.yaml
-cd ../../../../../../..
