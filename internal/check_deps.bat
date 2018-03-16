0efc3bcae416e4b68e5bfe92b57f0179e987c5ad
peter <peter_jiachen@163.com>
2018-03-16 19:04:59 +0800

    Fix #23

--- internal/check_deps.bat
+++ internal/check_deps.bat
@@ -21,8 +21,8 @@ IF NOT EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -legacy -version [15^,16^) -property installationPath`) do (
     if exist "%%i" if exist "%%i\VC\Auxiliary\Build\vcvarsall.bat" (
-        set VS15INSTALLDIR=%%i
-        set VS15VCVARSALL=%%i\VC\Auxiliary\Build\vcvarsall.bat
+        set "VS15INSTALLDIR=%%i"
+        set "VS15VCVARSALL=%%i\VC\Auxiliary\Build\vcvarsall.bat"
         goto vswhere
     )
 )
