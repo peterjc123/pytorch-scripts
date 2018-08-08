711bdd24187ab3597a0bd65ad930700b4e1ab3e1
peter <peter_jiachen@163.com>
2018-08-08 12:34:54 +0800

    Add cuda92 build script

---
+++ cuda92.bat
@@ -0,0 +1,39 @@
+@echo off
+
+IF NOT EXIST "setup.py" IF NOT EXIST "pytorch" (
+    call internal\clone.bat
+    cd ..
+    IF ERRORLEVEL 1 goto eof
+)
+
+call internal\check_deps.bat
+IF ERRORLEVEL 1 goto eof
+
+REM Check for optional components
+
+set NO_CUDA=
+set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
+
+IF "%NVTOOLSEXT_PATH%"=="" (
+    echo NVTX ^(Visual Studio Extension ^for CUDA^) ^not installed, disabling CUDA
+    set NO_CUDA=1
+    goto optcheck
+)
+
+IF "%CUDA_PATH_V9_2%"=="" (
+    echo CUDA 9.2 not found, disabling it
+    set NO_CUDA=1
+) ELSE (
+    set "CUDA_PATH=%CUDA_PATH_V9_2%"
+    set "PATH=%CUDA_PATH_V9_2%\bin;%PATH%"
+)
+
+:optcheck
+
+call internal\check_opts.bat
+IF ERRORLEVEL 1 goto eof
+
+call internal\setup.bat
+IF ERRORLEVEL 1 goto eof
+
+:eof
