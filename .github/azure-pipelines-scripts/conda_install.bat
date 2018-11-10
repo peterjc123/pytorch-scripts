e33d7d453d93ce713f152b69a23aa7867c7bdea5
peter <peter_jiachen@163.com>
2018-11-10 12:46:16 +0800

    .

--- .github/azure-pipelines-scripts/conda_install.bat
+++ .github/azure-pipelines-scripts/conda_install.bat
@@ -4,13 +4,13 @@
 set "CONDA_HOME=%CD%\conda"
 set "tmp_conda=%CONDA_HOME%"
 set "miniconda_exe=%CD%\miniconda.exe"
-rmdir /s /q conda
-del miniconda.exe
+if exist conda rmdir /s /q conda
+if exist miniconda.exe del miniconda.exe
 
 curl -k https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe -o "%miniconda_exe%"
 if errorlevel 1 exit /b 1
 
-call ..\conda\install_conda.bat
+start /wait "" "%miniconda_exe%" /S /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /D=%tmp_conda%
 if errorlevel 1 exit /b 1
 
 set "PATH=%CONDA_HOME%;%CONDA_HOME%\scripts;%CONDA_HOME%\Library\bin;%PATH%"
