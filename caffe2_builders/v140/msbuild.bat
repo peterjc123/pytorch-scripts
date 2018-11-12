687ce49130ce0fb438eafc3d7533ba7c1cff9222
ArutyunovG <ArutyunovG@yandex.ru>
2018-11-12 06:59:08 +0300

    Add Caffe2 building scripts (#44)
    * Caffe2 building script with Visual Studio 2015.
    
    * caffe2 v140 build system refactoring.
    
    * Restore auto.bat and msbuild.bat after merging peterjc123/master.
    
    * Changing dir before and after pytorch clone.
    
    * Test caffe2 build
    
    * Added dependencies for v140\auto.bat
    
    GFlags;
    GLog;
    LMDB;
    OpenCV.

---
+++ caffe2_builders/v140/msbuild.bat
@@ -0,0 +1,32 @@
+if not exist %CAFFE2_ROOT%\build\%CONFIG% (
+    mkdir %CAFFE2_ROOT%\build\%CONFIG%
+)   
+cd %CAFFE2_ROOT%\build\%CONFIG%
+
+if %CMAKE_GENERATOR% EQU "Visual Studio 14 2015 Win64" (
+
+    cmake %CAFFE2_ROOT% -G%CMAKE_GENERATOR% ^
+                             -DUSE_OPENCV=ON ^
+                             -DCMAKE_BUILD_TYPE=%CONFIG% ^
+                             -DCMAKE_INSTALL_PREFIX=%CAFFE2_ROOT%\build\%CONFIG%\install ^
+                             -DCMAKE_CXX_COMPILER=%CXX% ^
+                             -DCMAKE_C_COMPILER=%CC% ^
+                             -DCMAKE_LINKER=%CMAKE_LINER% ^
+                             -DINCLUDE_EXPERIMENTAL_C10_OPS=OFF ^
+                             -DBUILD_SHARED_LIBS=ON ^
+                             -DBUILD_BINARY=ON
+                                 
+    if errorlevel 1 exit /b 1
+    
+    call %VC_BIN_ROOT%\vcvars64.bat
+            
+) else (
+    echo "Error: Script supports only Visual Studio 14 2015 Win64 generator"
+    echo "Exiting..."
+    cd %ORIGINAL_DIR%
+    exit
+)
+
+msbuild /p:Configuration=%CONFIG% /p:Platform=x64 /m:%MAX_JOBS% INSTALL.vcxproj /p:PreferredToolArchitecture=x64
+if errorlevel 1 exit /b 1
+
