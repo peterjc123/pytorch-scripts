1f0e72a7cbe13062583e1572fc569c193440723b
Grigory Arutyunov <grigory.arutyunov@iss.ru>
2018-11-15 12:01:51 +0300

    Change c10 exerimental ops option name

--- caffe2_builders/v140/msbuild.bat
+++ caffe2_builders/v140/msbuild.bat
@@ -12,7 +12,7 @@ if %CMAKE_GENERATOR% EQU "Visual Studio 14 2015 Win64" (
                              -DCMAKE_CXX_COMPILER=%CXX% ^
                              -DCMAKE_C_COMPILER=%CC% ^
                              -DCMAKE_LINKER=%CMAKE_LINER% ^
-                             -DINCLUDE_EXPERIMENTAL_C10_OPS=OFF ^
+                             -DBUILD_C10_EXPERIMENTAL_OPS=OFF ^
                              -DBUILD_SHARED_LIBS=ON ^
                              -DBUILD_BINARY=ON
                                  
